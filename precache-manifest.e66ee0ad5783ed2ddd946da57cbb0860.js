841cf965a219b96c739c2d9fb500d2ba26956bed
Mudassar Bhatti <mbhatti000@citymail.cuny.edu>
2019-10-01 15:40:34 -0400

    Updates

---
+++ precache-manifest.e66ee0ad5783ed2ddd946da57cbb0860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335b0b80c0cfb15e6f8e4b8fac661705",
+    "url": "/serverlessApp/index.html"
+  },
+  {
+    "revision": "b9fdea9adebfa90a0c17",
+    "url": "/serverlessApp/static/css/main.31a5594f.chunk.css"
+  },
+  {
+    "revision": "5290a62cceb303f00c98",
+    "url": "/serverlessApp/static/js/2.2b1899d7.chunk.js"
+  },
+  {
+    "revision": "b9fdea9adebfa90a0c17",
+    "url": "/serverlessApp/static/js/main.5b4abffe.chunk.js"
+  },
+  {
+    "revision": "a17bf03588c0e25738db",
+    "url": "/serverlessApp/static/js/runtime-main.cb9e0f37.js"
+  }
+]);
